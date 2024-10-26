940bec8addf7b2996ebfcf741b4568dcc961064b
SHAWN_SJ <shawnsj9003@gmail.com>
2024-10-26 12:11:45 +0530

    Function call

--- kaman_mpu6050.cpp
+++ kaman_mpu6050.cpp
@@ -1,14 +1,9 @@
 #include <Wire.h>
 #include <MPU6050.h>
+#include <SimpleKalmanFilter.h> // Import the Kalman library
 
 MPU6050 gyro;
-
-float kalmanFilterX(float measurement);
-
-float Q = 0.01;
-float R = 0.1;
-float P_x = 1;
-float x_hat_x = 0;
+SimpleKalmanFilter kalmanFilter(0.01, 0.1, 1); // Initialize the Kalman filter with Q, R, and initial P values
 
 void setup()
 {
@@ -23,19 +18,11 @@ void loop()
     gyro.getAcceleration(&ax, &ay, &az);
 
     float ax_g = ax / 16384.0;
-    float filtered_x = kalmanFilterX(ax_g);
+    float filtered_x = kalmanFilter.updateEstimate(ax_g); // Pass the raw measurement
 
     Serial.print(ax_g);
     Serial.print(", ");
     Serial.println(filtered_x);
 
     delay(500);
-}
-
-float kalmanFilterX(float measurement)
-{
-    float K = P_x / (P_x + R);
-    x_hat_x = x_hat_x + K * (measurement - x_hat_x);
-    P_x = (1 - K) * P_x + Q;
-    return x_hat_x;
 }
